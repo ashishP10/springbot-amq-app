f4edb385c1f9b835b8fa2675f7911a3c0fb2ad55
Ashish <ashish.ranjan@people10.com>
2023-12-06 01:19:00 +0530

    initial commit with jms configuration

---
+++ springboot-amq-app/src/main/java/com/springboot/app/SpringbootAmqAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAmqAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAmqAppApplication.class, args);
+	}
+
+}
